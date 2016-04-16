1a06ee20821268e87b0e9dff9a557519a27e7067
Piyush Sharma <piyushnet@live.com>
2016-04-16 17:35:23 -0400

    Project Manager Added.

---
+++ Automation Studio/Project Manager/ProjectManager.cs
@@ -0,0 +1,21 @@
+﻿using Automation_Studio.Project_Manager.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_Studio.Project_Manager
+{
+    class ProjectManager
+    {
+
+        AutomationStudioProject Open(String FilePath)
+        {
+            AutomationStudioProject _Project = new AutomationStudioProject();
+
+            return _Project;
+
+        }
+    }
+}
---
+++ Automation Studio/Project Manager/XML Serializer/XMLSerializer.cs
@@ -0,0 +1,14 @@
+﻿using Automation_Studio.Project_Manager.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_Studio.Project_Manager
+{
+    class XMLSerializer
+    {
+        
+    }
+}
